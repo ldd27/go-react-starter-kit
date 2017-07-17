79c493ae83ae1ba3ec5ca07b6ed5b6d8f39b0e91
ldd911127 <365037133@qq.com>
2017-07-17 19:32:02 +0800

    use echo

--- main.go
+++ main.go
@@ -3,25 +3,26 @@ package main
 import (
 	_ "github.com/jdongdong/go-react-starter-kit/routers"
 
-	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/plugins/cors"
-	"github.com/jdongdong/go-react-starter-kit/controllers"
+	"github.com/jdongdong/go-lib/slog"
+	"github.com/labstack/echo"
 )
 
 func main() {
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	}
-
-	//解决跨域问题
-	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		AllowOrigins:     []string{beego.AppConfig.String("accessdomain")},
-		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "token", "X-Requested-With"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-	}))
-	beego.ErrorController(&controllers.ErrorController{})
-	beego.Run()
+	e := echo.New()
+	slog.Error(e.Start(":8080"))
+	//if beego.BConfig.RunMode == "dev" {
+	//	beego.BConfig.WebConfig.DirectoryIndex = true
+	//	beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	//}
+	//
+	////解决跨域问题
+	//beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+	//	AllowOrigins:     []string{beego.AppConfig.String("accessdomain")},
+	//	AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
+	//	AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "token", "X-Requested-With"},
+	//	ExposeHeaders:    []string{"Content-Length"},
+	//	AllowCredentials: true,
+	//}))
+	//beego.ErrorController(&controllers.ErrorController{})
+	//beego.Run()
 }
